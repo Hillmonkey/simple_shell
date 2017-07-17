693df9e5ff6379d0209e248da0b4d725c528a250
Larry Madeo <lmadeo76@gmail.com>
2017-07-17 20:58:57 +0000

    increment counter to put in error msgs

--- exec_cmd.c
+++ exec_cmd.c
@@ -6,7 +6,10 @@
 **/
 void exec_cmd(shenv_t *se)
 {
+	char alpha_counter[30];
+
 	tokenize(se);
+	(se->counter)++;
 	if (se->linelen > 0)
 	{
 		switch (se->pid = fork())
@@ -15,7 +18,16 @@ void exec_cmd(shenv_t *se)
 				perror("fork()");
 			case 0: /* child */
 				se->status = execve(se->cmd_tokens[0], se->cmd_tokens, NULL);
-				exit(se->status);
+				if (_strlen(se->cmd_tokens[0]) > 0)
+				{
+					_puts_err("hsh: ");
+					itoa(se->counter, alpha_counter);
+					_puts_err(alpha_counter);
+					_puts_err(": ");
+					_puts_err(se->cmd_tokens[0]);
+					perror(" ");
+					exit(se->status);
+				}
 			default: /* parent */
 				if (waitpid(se->pid, &(se->status), 0) < 0)
 				{
