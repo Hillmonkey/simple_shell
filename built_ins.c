77665a519629884d8e558ac20144b5df686c2ff4
wendysegura <118@holbertonschool.com>
2017-07-18 20:43:40 +0000

    created env builtin

--- built_ins.c
+++ built_ins.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 
 /**
-**
-**
+**built_exit - exit builtin
+**@se: shell env
+**Return: exit
 **/
 int built_exit(shenv_t *se)
 {
@@ -12,13 +13,30 @@ int built_exit(shenv_t *se)
 }
 
 /**
-**
-**
+**built_p_env - environment builtin
+**@se: shell envi
+**Return: the env
 **/
-int built_env(shenv_t *se)
+int built_p_env(shenv_t *se)
 {
 	UNUSED(se);
-	_puts("built_env\n");
+
+	int i = 0;
+	
+	_puts("built_p_env\n");
+
+	if (environ == NULL)
+	{
+		perror("hsh environment list is NULL");
+		return (EXIT_FAILURE);
+
+	}
+	
+	for (; environ[i] != NULL; i++)
+	{
+		write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
+		write(STDOUT_FILENO, "\n", 1);
+	}	
 	return (EXIT_SUCCESS);
 }
 
