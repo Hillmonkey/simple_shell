4190179dfc2ee1ece2feca5836300bfee2a777a9
Larry Madeo <lmadeo76@gmail.com>
2017-07-19 03:07:35 +0000

    added exec_abs_cmd

--- exec_cmd.c
+++ exec_cmd.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
+
 /**
 ** exec_cmd - executes commands
-** @se: arg
+** @se: Shell Environment struct
 ** Return: void
 **/
 void exec_cmd(shenv_t *se)
@@ -16,6 +17,40 @@ void exec_cmd(shenv_t *se)
 			case 0: /* child */
 				se->status = execve(se->cmd_tokens[0], se->cmd_tokens, NULL);
 				if (_strlen(se->cmd_tokens[0]) > 0)
+				{
+					err_msg(se);
+					perror("");
+					exit(se->status);
+				}
+			default: /* parent */
+				if (waitpid(se->pid, &(se->status), 0) < 0)
+				{
+					perror("waitpid()");
+					exit(EXIT_FAILURE);
+				}
+		}
+	}
+	else if (is_interactive(STDIN_FILENO, se->buf))
+		printf("\n");
+}
+
+/**
+** exec_abs_cmd - executes command with built token[0]
+** @se: Shell Environmnet struct
+** Return: void
+**/
+void exec_abs_cmd(shenv_t *se, char *arg0)
+{
+	(se->counter)++;
+	if (se->linelen > 0)
+	{
+		switch (se->pid = fork())
+		{
+			case -1:
+				perror("fork()");
+			case 0: /* child */
+				se->status = execve(arg0, se->cmd_tokens, NULL);
+				if (_strlen(arg0) > 0)
 				{
 					err_msg(se);
 					perror("");
